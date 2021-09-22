c7cd2ad50dd3ae193d44d86405ca287bbb20107d
chareezy <charr.lam@gmail.com>
2021-09-21 17:53:58 -0700

    removed trim.toUppercase and added it to css

--- src/index.js
+++ src/index.js
@@ -127,7 +127,7 @@ function search(city) {
 function handleSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text");
-  search(searchInput.value.trim().toUpperCase());
+  search(searchInput.value);
 }
 let form = document.querySelector("#search-city");
 form.addEventListener("submit", handleSubmit);
@@ -145,7 +145,7 @@ function showPosition(position) {
   axios.get(apiUrl).then(showCity);
 }
 function showCity(response) {
-  let currentCity = response.data.name.trim().toUpperCase();
+  let currentCity = response.data.name;
   let city = document.querySelector("h1");
   city.innerHTML = `${currentCity}`;
 
